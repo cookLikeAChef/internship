1194fe88adda7ff76ba0168b2f46e2eb702b6191
cook <kwonjae13@gmail.com>
2017-07-14 20:41:56 +0900

    initialize

--- untitled/main.py
+++ untitled/main.py
@@ -1,7 +1,7 @@
 import myPlot as mp
-import scipy
+from scipy.stats import norm
 
-with open("./dataset/sample1.txt", 'r') as f: # 파일을 읽어들임
+with open("./dataset/sample2.txt", 'r') as f: # 파일을 읽어들임
     data = f.read().split(',') # 콤마(,)를 기준으로 문자열을 나누어 리스트형식으로 리턴
 
 # 문자형 데이터를 숫자형으로 변환
@@ -10,6 +10,6 @@ for i in range(lengthOfData):
     data[i] = float(data[i])
 
 percentile = 0.9997
-tmp = scipy.stats.norm.ppf(.97)
+sigmaValue = norm.ppf(percentile)
 #mp.createPlot(data, windowSize = 30, sigma = 3.0, rolling = True)
-# mp.createPlot(data, windowSize = 30, sigma = test_sigma, rolling = True)
+mp.createPlot(data, windowSize = 30, sigma = sigmaValue, rolling = True)
